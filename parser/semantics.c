4c3a395613f29f9cc5d5dd96a6253fed0be88f31
Erik Báča <zeryk.baca@seznam.cz>
2020-11-25 17:04:48 +0100

    added symTabAdding

--- parser/semantics.c
+++ parser/semantics.c
@@ -5,6 +5,46 @@ tToken *getValue(Tree *tree)
     return tree->value;
 }
 
+
+int symTabAdd(Scope *scope, tToken *name, Tree *Value)
+{
+    Data *data = malloc(sizeof(struct Data_struct));
+    data->defined = TRUE;
+    data->paramsNumber = 0;
+    while (Value->LPtr != NULL)
+    {
+        Value = Value->LPtr;
+    }
+    if(Value->value->id==ID_IDENTIFIER)
+    {
+        Data dataType;
+        Search(scope->table,Value->value->att.s, &dataType);
+        data->type = dataType.type;
+    } else
+        data->type = Value->value->id;
+    Insert(&scope->table,name->att.s,data);
+}
+
+int identifierScopeCheck(Scope *scope, tToken *term, tToken *Value)
+{
+    Data data;
+    if(Search(scope->table,term->att.s, &data))
+    {
+        return TRUE;
+    } 
+    else
+    {
+        if(scope->prev != NULL)
+            identifierScopeCheck(scope->prev, term, Value);
+    }
+    return FALSE;
+} 
+
+//int identifierValueCheck(Scope *scope, Tree *Value)
+//{
+
+//}
+
 Tree *makeLeaf(tToken *term)
 {
     Tree *newTree = malloc(sizeof(struct T));
@@ -15,10 +55,15 @@ Tree *makeLeaf(tToken *term)
     return newTree;
 }
 
-Tree *makeTree(Tree *x, Tree *y, tToken *term)
+Tree *makeTree(Tree *x, Tree *y, tToken *op, Scope *scope)
 {
+    if(op->id == ID_DEFINE)
+        symTabAdd(scope, x->value,y);
+    //if(op->id == ID_ASSIGN)
+        //identifierScopeCheck(scope,x, y);
+    //if(identifierCheck(scope))
     Tree *newTree = malloc(sizeof(struct T));
-    newTree->value = term;
+    newTree->value = op;
     newTree->LPtr = x;
     newTree->RPtr = y;
     return newTree;
