37f2c184e02569b8af949d0231ce6f354cdbbd26
Jacob-147 <jacob.novotny@seznam.cz>
2020-11-27 15:11:55 +0100

    Generator implementation
    * Added generator for expression (operation code)

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include "./parser/parser.h"
+#include "./generator/generator.h"
+
 #include <stdio.h>
 int main()
 {
-	Parse();
+	//Parse();
 	printf("Program compiled! yay\n");
+
+
+	//Exp *Exp = Parse();
+
+
+	Exp *exp = Parse();
+	Exp *root = NULL;
+	root = exp;
+	printf("----------Generator start ----------\n");
+	generator(exp, root);
+	printf("------------Generator exit!---------\n");
+
+
+
 	return 0;
 }
