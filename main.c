4f78da2cde1d6056f53f0fc46f740a6a66220442
Michal <misa.zavadil@gmail.com>
2020-12-05 15:38:11 +0100

    Added print_tree

--- main.c
+++ main.c
@@ -3,6 +3,35 @@
 #include "debug.h"
 #include "error.h"
 #include <stdio.h>
+
+void print_tree(Exp *exp, int spaces_num)
+{
+	print_token(exp->value);
+	spaces_num++;
+	if (exp->Condition != NULL)
+	{
+		for (int i = 0; i < spaces_num; i++)
+			printf("      ");
+		printf("Cond->  ");
+		print_tree(exp->Condition, spaces_num);		
+	}
+	if (exp->RPtr != NULL)
+	{
+		for (int i = 0; i < spaces_num; i++)
+			printf("      ");
+		printf("R->  ");
+		print_tree(exp->RPtr, spaces_num);
+	}
+	if (exp->LPtr != NULL)
+	{
+		for (int i = 0; i < spaces_num; i++)
+			printf("      ");
+		printf("L->  ");
+		print_tree(exp->LPtr, spaces_num);
+	}
+	return;
+}
+
 int main()
 {
 	//Parse();
@@ -10,6 +39,10 @@ int main()
 	Exp *exp = Parse();
 	Exp *root = NULL;
 	root = exp;
+
+	int spaces_num = 0;
+	print_tree(exp, spaces_num);
+	
 	DEBUG_PRINT(("----------Generator start ----------\n"));
 	generator(exp, root);
 	DEBUG_PRINT(("------------Generator exit!---------\n"));
