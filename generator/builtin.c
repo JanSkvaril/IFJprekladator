78954eef8cb78c3ef5aa55a7b3775513224ff65f
Michal <misa.zavadil@gmail.com>
2020-12-07 18:31:58 +0100

    Substr built-in function, fixes in other built-ins

--- generator/builtin.c
+++ generator/builtin.c
@@ -2,6 +2,9 @@
 #include "builtin.h"
 #include "generator.h"
 
+#define REP2(X) X,X
+#define REP3(X) X,X,X
+
 static int built_counter = 0;
 
 void built_inputx(Exp *retvals, char *type)
@@ -95,7 +98,70 @@ void built_len(Exp *params, Exp *retvals)
 
 void built_substr(Exp *params, Exp *retvals)
 {
-	
+	tTokenPtr retval0 = retvals->RPtr->value;
+	tTokenPtr retval1 = retvals->LPtr->value;
+	tTokenPtr param0 = params->RPtr->value;
+	tTokenPtr param1 = params->LPtr->RPtr->value;
+	tTokenPtr param2 = params->LPtr->LPtr->value;
+
+	if(retval1->id != ID_UNDER)
+		printf("MOVE LF@%s int@1\n", retval1->att.s);
+
+	printf("DEFVAR LF@*str$%d\n", built_counter);
+	printf("MOVE LF@*str$%d ", built_counter);
+	if (param0->id == ID_IDENTIFIER)
+		printf("LF@%s\n", param0->att.s);
+	else
+	{
+		print_string_lit(param0->att.s);
+		printf("\n");
+	}
+
+	printf("DEFVAR LF@*iter$%d\n", built_counter);
+	printf("MOVE LF@*iter$%d ", built_counter);
+	if (param1->id == ID_IDENTIFIER)
+		printf("LF@%s\n", param1->att.s);
+	else
+		printf("int@%ld\n", param1->att.i);
+
+	printf("DEFVAR LF@*len$%d\n", built_counter);
+	printf("MOVE LF@*len$%d ", built_counter);
+	if (param2->id == ID_IDENTIFIER)
+		printf("LF@%s\n", param2->att.s);
+	else
+		printf("int@%ld\n", param2->att.i);
+
+	//arg check
+	printf("DEFVAR LF@*tmp$%d\n", built_counter);
+	printf("DEFVAR LF@*sum$%d\n", built_counter);
+	printf("STRLEN LF@*tmp$%d LF@*str$%d\n", REP2(built_counter));
+	printf("ADD LF@*sum$%d LF@*len$%d LF@*iter$%d\n", REP3(built_counter));
+	printf("LT LF@*tmp$%d LF@*tmp$%d LF@*sum$%d\n", REP3(built_counter));
+	printf("JUMPIFEQ *end%d LF@*tmp$%d bool@true\n", REP2(built_counter));
+
+	if(retval1->id != ID_UNDER)
+		printf("MOVE LF@%s int@0\n", retval1->att.s);
+
+	//calculation
+	printf("DEFVAR LF@*x%d\n", built_counter);
+	printf("DEFVAR LF@*y%d\n", built_counter);
+	printf("GETCHAR LF@*x%d LF@*str$%d LF@*iter$%d\n", REP3(built_counter));
+
+	printf("SUB LF@*len$%d LF@*len$%d int@1\n", REP2(built_counter));
+	printf("JUMPIFEQ *once%d LF@*len$%d int@0\n", REP2(built_counter));
+	printf("LABEL *loop%d\n", built_counter);
+	printf("ADD LF@*iter$%d LF@*iter$%d int@1\n", REP2(built_counter));
+	printf("GETCHAR LF@*y%d LF@*str$%d LF@*iter$%d\n", REP3(built_counter));
+	printf("CONCAT LF@*x%d LF@*x%d LF@*y%d\n", REP3(built_counter));
+	printf("SUB LF@*len$%d LF@*len$%d int@1\n", REP2(built_counter));
+	printf("JUMPIFNEQ *loop%d LF@*len$%d int@0\n", REP2(built_counter));
+
+	printf("LABEL *once%d\n", built_counter);
+	printf("MOVE LF@%s LF@*x%d\n", retval0->att.s, built_counter);
+
+	printf("LABEL *end%d\n", built_counter);
+
+	built_counter++;
 	return;
 }
 
@@ -133,20 +199,24 @@ void built_ord(Exp *params, Exp *retvals)
 	if(retval1->id != ID_UNDER)
 		printf("MOVE LF@%s int@0\n", retval1->att.s);
 
-	printf("STRI2INT LF@%s ", retval0->att.s);
-	if (param0->id == ID_IDENTIFIER)
-		printf("LF@%s ", param0->att.s);
-	else
+	if(retval0->id != ID_UNDER)
 	{
-		print_string_lit(param0->att.s);
-		printf(" ");
+		printf("STRI2INT LF@%s ", retval0->att.s);
+		if (param0->id == ID_IDENTIFIER)
+			printf("LF@%s ", param0->att.s);
+		else
+		{
+			print_string_lit(param0->att.s);
+			printf(" ");
+		}
+		if (param1->id == ID_IDENTIFIER)
+			printf("LF@%s\n", param1->att.s);
+		else
+			printf("int@%ld\n", param1->att.i);
 	}
-	if (param1->id == ID_IDENTIFIER)
-		printf("LF@%s\n", param1->att.s);
-	else
-		printf("int@%ld\n", param1->att.i);
 
 	printf("LABEL *end%d\n", built_counter);
+	built_counter++;
 	return;
 }
 
@@ -174,13 +244,15 @@ void built_chr(Exp *params, Exp *retvals)
 	if(retval1->id != ID_UNDER)
 		printf("MOVE LF@%s int@0\n", retval1->att.s);
 
-	printf("INT2CHAR LF@%s ", retval0->att.s);
+	if(retval0->id != ID_UNDER)
+	{
+		printf("INT2CHAR LF@%s ", retval0->att.s);
 
-	if (params->value->id == ID_IDENTIFIER)
-		printf("LF@%s\n", param.s);
-	else
-		printf("int@%ld\n", param.i);
-
+		if (params->value->id == ID_IDENTIFIER)
+			printf("LF@%s\n", param.s);
+		else
+			printf("int@%ld\n", param.i);
+	}
 	printf("LABEL *end%d\n", built_counter);
 
 	built_counter++;
