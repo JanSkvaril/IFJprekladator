b91c4a24bebe1bcd310c74694937a74d3fd481aa
Jacob-147 <jacob.novotny@seznam.cz>
2020-12-09 19:11:36 +0100

    header space fix

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /**
-* NÁZEV PROJEKTU:		Implementace překladače imperativního jazyka IFJ20
-* ČÍSLO TÝMU:			115
-* VARIANTA:			1.
+* NÁZEV PROJEKTU:   Implementace překladače imperativního jazyka IFJ20
+* ČÍSLO TÝMU:       115
+* VARIANTA:         1.
 *
-* Michal Zavadil 		login: xzavad18
-* Jakub Novotný 		login: xnovot2a
-* Jan Škvařil 			login: xskvar09
-* Erik Báča 			login: xbacae00
+* Michal Zavadil    login: xzavad18
+* Jakub Novotný     login: xnovot2a
+* Jan Škvařil       login: xskvar09
+* Erik Báča         login: xbacae00
 */
 #include "./parser/parser.h"
 #include "./generator/generator.h"
